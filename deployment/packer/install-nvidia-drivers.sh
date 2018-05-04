9a7508063bab4d01325f9af37f8151fd7494dafe
Lewis Fishgold <lewfish@gmail.com>
2018-05-04 15:37:20 -0400

    Update AMI creation process
    Some stuff broke and needed tweaks

--- deployment/packer/install-nvidia-drivers.sh
+++ deployment/packer/install-nvidia-drivers.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -ex
 
 # Update nvidia drivers
-NVIDIA_DRIVERS_VERSION="375.51"
-
-wget "http://us.download.nvidia.com/XFree86/Linux-x86_64/${NVIDIA_DRIVERS_VERSION}/NVIDIA-Linux-x86_64-${NVIDIA_DRIVERS_VERSION}.run"
-sudo /bin/bash "./NVIDIA-Linux-x86_64-${NVIDIA_DRIVERS_VERSION}.run" -asq
+NVIDIA_DRIVERS_VERSION="384.125"
+wget "http://us.download.nvidia.com/tesla/${NVIDIA_DRIVERS_VERSION}/NVIDIA-Linux-x86_64-${NVIDIA_DRIVERS_VERSION}.run"
+sudo /bin/bash "./NVIDIA-Linux-x86_64-${NVIDIA_DRIVERS_VERSION}.run" -sq --install-libglvnd
 sudo reboot
-
